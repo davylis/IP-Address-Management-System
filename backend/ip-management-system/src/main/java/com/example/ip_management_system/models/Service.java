3fe120d21139a4bce9e29233abd40595303fb666
Liisa Davydov <liisamuttonen@gmail.com>
2024-11-15 15:48:46 +0200

    Add IP Service entity class

---
+++ backend/ip-management-system/src/main/java/com/example/ip_management_system/models/Service.java
@@ -0,0 +1,141 @@
+package com.example.ip_management_system.models;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "services")
+public class Service {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "ipaddress_id", nullable = false)
+    private IpAddress ipAddress;
+
+    @Column(nullable = false)
+    private int port;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = true)
+    private String description;
+
+    @Column(nullable = true)
+    private String urlLink;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private ServiceStatus status;
+
+
+    /**
+     * @return Long return the id
+     */
+    public Long getId() {
+        return id;
+    }
+
+    /**
+     * @param id the id to set
+     */
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    /**
+     * @return IpAddress return the ipAddress
+     */
+    public IpAddress getIpAddress() {
+        return ipAddress;
+    }
+
+    /**
+     * @param ipAddress the ipAddress to set
+     */
+    public void setIpAddress(IpAddress ipAddress) {
+        this.ipAddress = ipAddress;
+    }
+
+    /**
+     * @return int return the port
+     */
+    public int getPort() {
+        return port;
+    }
+
+    /**
+     * @param port the port to set
+     */
+    public void setPort(int port) {
+        this.port = port;
+    }
+
+    /**
+     * @return String return the name
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * @param name the name to set
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    /**
+     * @return String return the description
+     */
+    public String getDescription() {
+        return description;
+    }
+
+    /**
+     * @param description the description to set
+     */
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    /**
+     * @return String return the urlLink
+     */
+    public String getUrlLink() {
+        return urlLink;
+    }
+
+    /**
+     * @param urlLink the urlLink to set
+     */
+    public void setUrlLink(String urlLink) {
+        this.urlLink = urlLink;
+    }
+
+    /**
+     * @return ServiceStatus return the status
+     */
+    public ServiceStatus getStatus() {
+        return status;
+    }
+
+    /**
+     * @param status the status to set
+     */
+    public void setStatus(ServiceStatus status) {
+        this.status = status;
+    }
+
+}
